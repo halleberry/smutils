769a19083a5ba178e52a298a15e50d816e6ca01d
halleberry <26785847+halleberry@users.noreply.github.com>
2024-07-02 01:21:18 -0600

    Update combine_pdfs.py with _merge and _reduce size functions

--- combine_pdfs.py
+++ combine_pdfs.py
@@ -1,6 +1,6 @@
 import os
 
-# pip install pypdf
+# pip install "pypdf[image]"
 from pypdf import PdfWriter
 
 
@@ -11,14 +11,19 @@ def combine_pdfs(infiles, outfile):
     Args:
         infiles (list): List of file paths of the PDF files to be combined.
         outfile (str): File path of the output PDF file.
-    
+
     >>> _infiles = ['~/Pictures/1.pdf', '~/Pictures/2.pdf', '~/Pictures/3.pdf', '~/Pictures/4.pdf']
     ... _outfile = '~/Pictures/combined.pdf'
     ... combine_pdfs(_infiles, _outfile)
-    
+
     Returns:
         None
     """
+    
+    _merge(infiles, outfile)
+
+
+def _merge(infiles, outfile):
     outfile = os.path.expanduser(outfile)
     if not os.path.exists(outfile):
         os.makedirs(os.path.dirname(outfile), exist_ok=True)
@@ -32,6 +37,24 @@ def combine_pdfs(infiles, outfile):
         else:
             print(f"Skipping non-PDF file: {infile}")
 
-    merger.write(outfile)
+    outfile_tbd = os.path.join(os.path.dirname(outfile), 'tbd.pdf')
+    merger.write(outfile_tbd)
     merger.close()
+
+    _reduce_size(infile=outfile_tbd, outfile=outfile)
+
+
+def _reduce_size(infile, outfile):
+    writer = PdfWriter(clone_from=infile)
+
+    for _page in writer.pages:
+        _page.compress_content_streams(1)
+
+    with open(outfile, "wb") as f:
+        writer.write(f)
+
+    writer.close()
+
+
+
 

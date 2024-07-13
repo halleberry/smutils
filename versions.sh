d25602c0caea277eb0a3f3e253746a0711065197
halleberry <26785847+halleberry@users.noreply.github.com>
2024-07-12 21:41:11 -0600

    Update versions.sh
    create git tag
    read git tags with info

--- versions.sh
+++ versions.sh
@@ -15,26 +15,31 @@ echo "v0.0.06 create web.app.api(controller): api"
 
 
 # ========================================================================
-# VERSION_git_tag_________________________________________________________
+# VERSION_git_tag_CREATE__________________________________________________
 # ========================================================================
 git add . 
 git commit -m "v0.0.01 init $(basename $(PWD)) microservice"
-
-
+#
 # git the hash of that commit with $(git rev-parse --short HEAD)
 _TARGET_COMMIT_HASH='32cd40d'
-
-
+#
 # go back to the target commit and create a tag
 _TAG_NAME='v0.0.01'
 git checkout "${_TARGET_COMMIT_HASH}"
-
-
+#
 # create tag
 git tag ${_TAG_NAME} -m "$(git rev-parse --short HEAD) $(date) $(git log -1 --pretty=%B)"
-
-
+#
+# ========================================================================
+# VERSION_git_tag_READ____________________________________________________
+# ========================================================================
 # display tag comment: eg (v0.0.01    32cd40d  Fri Jul 12 21:00:58 MDT 2024  v0.0.01 init multi_language_strings microservice)
 git tag -n1 v0.0.01
+#
+# list tags with comments
+git tag -n
+
+
+
 
 

a687b25cb218620c055c33027714a95033d46ad5
halleberry <26785847+halleberry@users.noreply.github.com>
2024-07-18 16:54:39 -0600

    Create microsvc_setup.py

---
+++ microsvc_setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='multi_language_strings',
+    description='multi_language_strings API manager',
+    version='0.0.1',
+    py_modules=['multi_language_strings']
+)
